73c772493079c0d00005af10c3a91588d1be985e
RRG <rohan.renji158@gmail.com>
2025-08-04 12:29:01 +0530

    sparsematrix.c

--- sparsematrix.c
+++ sparsematrix.c
@@ -1,106 +1,110 @@
-#include<stdio.h>
+#include <stdio.h>
+
 #define MAX 100
 
-void sumSparseMatrices(int mat1[MAX][3], int mat2[MAX][3], int result[MAX][3], int *r1, int *r2)
-{
-  int i=0, j=0, k=0;
-  while (i<*r1 && j<*r2)
-  {
-    if (mat1[i][0]==mat2[j][0] && mat1[i][1]==mat2[j][1])
-    {
-      result[k][0]=mat1[i][0];
-      result[k][1]=mat1[i][1];
-      result[k][2]=mat1[i][2]+mat2[j][2];
-      i++;
-      j++;
-    }
-    else if (mat1[i][0] < mat2[j][0] || (mat1[i][0] == mat2[j][0] && mat1[i][1]<mat2[j][1]))
-    {
-      result[k][0]=mat1[i][0];
-      result[k][1]=mat1[i][1];
-      result[k][2]=mat1[i][2];
-      i++;
-      j++;
-    }
-    else
-    {
-      result[k][0]=mat1[i][0];
-      result[k][1]=mat1[i][1];
-      result[k][2]=mat2[j][2];
-      i++;
-      j++;
-    }
-  }
-  while (i < *r1)
-  {
-      result[k][0]=mat1[i][0];
-      result[k][1]=mat1[i][1];
-      result[k][2]=mat1[i][2];
-      i++;
-      j++;
-  }
-  while (j < *r2) 
-  {
-     result[k][0]=mat1[i][0];
-      result[k][1]=mat1[i][1];
-      result[k][2]=mat2[j][2];
-      i++;
-      j++;
-  }
-  *r1 = k;
-}
-void printSparseMatrix(int matrix[MAX][3], int rows) 
-{
-   printf("\nRow Column Value\n");
-   for(int i=0; i<rows; i++)
-    {
-      printf("%d    %d    %d\n", matrix[i][0], matrix[i][1], matrix[i][2]);
-    }
-}
-void inputSparseMatrix(int matrix[MAX][3], int *rows) 
-{
-  int i;
-  printf("Enter the number of non-zero elements in the matrix: ");
-  scanf("%d", &*rows);
-  for (int i = 0; i < *rows; i++) 
-  {
-      printf("Enter row index, column index and value for element %d: ", i + 1);
-      scanf("%d %d %d", &matrix[i][0], &matrix[i][1], &matrix[i][2]);
-  }
-}
-void transposeSparseMatrix(int matrix[MAX][3], int result[MAX][3], int rows) {
-    int k = 0;
-    for (int col = 0; col < MAX; col++) {
-        for (int i = 0; i < rows; i++) {
-            if (matrix[i][1] == col) {
-                result[k][0] = matrix[i][1]; 
-                result[k][1] = matrix[i][0]; 
-                result[k][2] = matrix[i][2];
-                k++;
-            }
+void addSparseMatrices(int matrix1[MAX][3], int matrix2[MAX][3], int result[MAX][3], int *r1, int *r2) {
+    int i = 0, j = 0, k = 0;
+
+    while (i < *r1 && j < *r2) {
+        if (matrix1[i][0] == matrix2[j][0] && matrix1[i][1] == matrix2[j][1]) {
+            result[k][0] = matrix1[i][0];
+            result[k][1] = matrix1[i][1];
+            result[k][2] = matrix1[i][2] + matrix2[j][2];
+            i++;
+            j++;
+            k++;
+        }
+        else if (matrix1[i][0] < matrix2[j][0] || (matrix1[i][0] == matrix2[j][0] && matrix1[i][1] < matrix2[j][1])) {
+            result[k][0] = matrix1[i][0];
+            result[k][1] = matrix1[i][1];
+            result[k][2] = matrix1[i][2];
+            i++;
+            k++;
+        }
+        else {
+            result[k][0] = matrix2[j][0];
+            result[k][1] = matrix2[j][1];
+            result[k][2] = matrix2[j][2];
+            j++;
+            k++;
         }
     }
+    
+    while (i < *r1) {
+        result[k][0] = matrix1[i][0];
+        result[k][1] = matrix1[i][1];
+        result[k][2] = matrix1[i][2];
+        i++;
+        k++;
+    }
+
+    while (j < *r2) {
+        result[k][0] = matrix2[j][0];
+        result[k][1] = matrix2[j][1];
+        result[k][2] = matrix2[j][2];
+        j++;
+        k++;
+    }
+
+    *r1 = k; 
 }
-int main()
-{
-    int mat1[MAX][3], mat2[MAX][3], result[MAX][3], transpose[MAX][3];
+
+void inputMatrix(int matrix[MAX][3], int *rows) {
+    int i;
+    printf("Enter the number of non-zero elements: ");
+    scanf("%d", rows);
+
+    for (i = 0; i < *rows; i++) {
+        printf("Enter row, column, and value for element %d: ", i + 1);
+        scanf("%d %d %d", &matrix[i][0], &matrix[i][1], &matrix[i][2]);
+    }
+}
+
+void printMatrix(int matrix[MAX][3], int rows) {
+    printf("\nRow  Column  Value\n");
+    for (int i = 0; i < rows; i++) {
+        printf("%d    %d      %d\n", matrix[i][0], matrix[i][1], matrix[i][2]);
+    }
+}
+void transposeSparseMatrix(int matrix[MAX][3], int *rows) {
+    int i;
+    int transposed[MAX][3];
+    int k = 0;
+
+    // Transposing the sparse matrix (swap rows and columns)
+    for (i = 0; i < *rows; i++) {
+        transposed[k][0] = matrix[i][1];  // Swap row and column
+        transposed[k][1] = matrix[i][0];  // Swap row and column
+        transposed[k][2] = matrix[i][2];  // Value remains the same
+        k++;
+    }
+    for (i = 0; i < k; i++) {
+        matrix[i][0] = transposed[i][0];
+        matrix[i][1] = transposed[i][1];
+        matrix[i][2] = transposed[i][2];
+    }
+
+    *rows = k;  // Update the number of rows (non-zero elements)
+}
+
+int main() {
+    int matrix1[MAX][3], matrix2[MAX][3], result[MAX][3];
     int rows1, rows2;
 
     printf("Enter details for the first sparse matrix:\n");
-    inputSparseMatrix(mat1, &rows1);
+    inputMatrix(matrix1, &rows1);
 
-    printf("Enter details for the second sparse matrix:\n");
-    inputSparseMatrix(mat2, &rows2);
+    printf("\nEnter details for the second sparse matrix:\n");
+    inputMatrix(matrix2, &rows2);
 
-    sumSparseMatrices(mat1, mat2, result, &rows1, &rows2);
+    addSparseMatrices(matrix1, matrix2, result, &rows1, &rows2);
 
     printf("\nResultant Sparse Matrix after Addition:\n");
-    printSparseMatrix(result, rows1);
+    printMatrix(result, rows1);
+    printf("\nTransposing the Resultant Sparse Matrix:\n");
+    transposeSparseMatrix(result, &rows1);
 
-    transposeSparseMatrix(result, transpose, rows1);
-
-    printf("\nTranspose of the Resultant Sparse Matrix:\n");
-    printSparseMatrix(transpose, rows1);
+    printMatrix(result, rows1);
 
     return 0;
 }
