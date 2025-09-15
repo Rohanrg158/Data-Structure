f04b9bb17bd2eae288d8d78e0270177f9c70b787
RRG <rohan.renji158@gmail.com>
2025-09-15 11:47:33 +0530

    Update Doubly_LL.c

--- Doubly_LL.c
+++ Doubly_LL.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 typedef struct Node {
     char url;
     struct Node* prev;
     struct Node* next;
 } Node;
-
 Node* create_node(const char* url) {
     Node* newNode = (Node*)malloc(sizeof(Node));
     strcpy(newNode->url, url);
@@ -15,9 +13,7 @@ Node* create_node(const char* url) {
     newNode->next = NULL;
     return newNode;
 }
-
 void visit(Node** curr, const char* url) {
-    // Remove possible forward nodes
     Node* temp = (*curr)->next;
     while (temp) {
         Node* toFree = temp;
@@ -25,49 +21,37 @@ void visit(Node** curr, const char* url) {
         free(toFree);
     }
     (*curr)->next = NULL;
-
     Node* newNode = create_node(url);
     newNode->prev = *curr;
     (*curr)->next = newNode;
     *curr = newNode;
 }
-
 void back(Node** curr) {
     if ((*curr)->prev) {
         *curr = (*curr)->prev;
-        printf("Moved Back: %s
-", (*curr)->url);
+        printf("Moved Back: %s", (*curr)->url);
     } else {
-        printf("No Back History.
-");
+        printf("No Back History.");
     }
 }
-
 void forward(Node** curr) {
     if ((*curr)->next) {
         *curr = (*curr)->next;
-        printf("Moved Forward: %s
-", (*curr)->url);
+        printf("Moved Forward: %s", (*curr)->url);
     } else {
-        printf("No Forward History.
-");
+        printf("No Forward History.");
     }
 }
-
 int main() {
     Node* homepage = create_node("home.com");
     Node* curr = homepage;
-
-    visit(&curr, "a.com");    // Visit a.com
-    visit(&curr, "b.com");    // Visit b.com
-    visit(&curr, "c.com");    // Visit c.com
-    
-    back(&curr);   // Go to b.com
-    back(&curr);   // Go to a.com
-    forward(&curr); // Go to b.com
-
-    visit(&curr, "d.com");  // Visit d.com, forward history deleted
-    forward(&curr);         // No forward history
-
+    visit(&curr, "a.com");   
+    visit(&curr, "b.com");    
+    visit(&curr, "c.com");    
+    back(&curr);   
+    back(&curr);   
+    forward(&curr); 
+    visit(&curr, "d.com");  
+    forward(&curr);       
     return 0;
 }
